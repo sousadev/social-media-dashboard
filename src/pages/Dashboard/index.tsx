1541989abe030db367b3d71f5ad13762a9b8b665
Italo Sousa <italo@italosousa.com.br>
2022-03-07 14:11:19 -0300

    Removed uniused item

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import Header from '../../components/Header';
 import { useTheme } from '../../contexts/Theme.context';
